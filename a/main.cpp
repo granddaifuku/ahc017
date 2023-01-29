193eef8e109f25a0fdfce379f82c949f59fe587b
granddaifuku <daifukuformac@gmail.com>
2023-01-29 13:55:22 +0900

    init

---
+++ a/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)n; ++i)
+#define FOR(i, a, b) for (int i = (int)a; i < (int)b; ++i)
+#define rrep(i, n) for (int i = ((int)n - 1); i >= 0; --i)
+
+using ll = long long;
+using ld = long double;
+
+__attribute__((unused)) const ll INF = 1e18;
+__attribute__((unused)) const int Inf = 1e9;
+__attribute__((unused)) const double EPS = 1e-9;
+__attribute__((unused)) const ll MOD = 1000000007;
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(0);
+
+  return 0;
+}
