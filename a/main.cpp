328791f922e03f83855a8029171145963ab7ecd4
granddaifuku <daifukuformac@gmail.com>
2023-02-02 22:20:34 +0900

    update

--- a/main.cpp
+++ a/main.cpp
@@ -14,11 +14,63 @@ __attribute__((unused)) const int Inf = 1e9;
 __attribute__((unused)) const double EPS = 1e-9;
 __attribute__((unused)) const ll MOD = 1000000007;
 
+// ================================================
+
+class DisjointSet {
+ public:
+  vector<int> rank, p, size;
+
+  DisjointSet() {}
+  DisjointSet(int s) {
+    rank.resize(s, 0);
+    p.resize(s, 0);
+    size.resize(s, 0);
+    rep(i, s) init(i);
+  }
+
+  void init(int x) {
+    p[x] = x;
+    rank[x] = 0;
+    size[x] = 1;
+  }
+
+  bool isSame(int x, int y) { return root(x) == root(y); }
+
+  void makeSet(int x, int y) {
+    if (isSame(x, y)) return;
+    link(root(x), root(y));
+  }
+
+  void link(int x, int y) {
+    if (rank[x] > rank[y]) {
+      p[y] = x;
+      size[x] += size[y];
+    } else {
+      p[x] = y;
+      size[y] += size[x];
+      if (rank[x] == rank[y]) {
+        rank[y]++;
+      }
+    }
+  }
+
+  int root(int x) {
+    if (x != p[x]) {
+      p[x] = root(p[x]);
+    }
+    return p[x];
+  }
+
+  int getSize(int x) { return size[root(x)]; }
+};
+
+// ================================================
+
 int N, M, D, K;
 
-vector<bool> repaired;
-
 using P = pair<int, int>;
+
+vector<P> connections;
 
 struct edge {
   int to;
@@ -51,35 +103,108 @@ void dijkstra(int s) {
 }
 
 // =================================================
-void solve() {
-  int e = 1;
-  rep(day, D) {
-    int count = 1;
-    while (e <= M) {
-      cout << day + 1;
-      count++, e++;
-      if (count < K) {
-        cout << " ";
-      } else {
-        cout << endl;
-        break;
-      }
+
+// TODO
+// 各エッジがどこを結ぶかを記録する
+// DFSで連結かどうか探索できる
+
+// 道路が接続されているかを確認する
+// Disjoint Set で連結かどうかを確認
+bool is_connected(const set<int>& repair) {
+  DisjointSet dj(N);
+  rep(i, M) {
+    if (repair.find(i) != repair.end()) {
+      continue;
     }
+    int a = connections[i].first, b = connections[i].second;
+    dj.makeSet(a, b);
   }
+
+  return dj.getSize(0) == N;
 }
 
-int main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(0);
+// 残り日数で全ての辺の修理が可能かどうか
+bool is_finishable(int edges, int days) { return edges <= days * K; }
 
-  // Input
+// ==================================================
+
+vector<int> solve() {
+  vector<int> result(M);
+  int remain_days = D;
+  int average = (M + D - 1) / D;
+
+  // 残っている修理が必要な辺
+  vector<int> tmp(M);
+  iota(tmp.begin(), tmp.end(), 0);
+  auto seed = chrono::system_clock::now().time_since_epoch().count();
+  shuffle(tmp.begin(), tmp.end(), default_random_engine(seed));
+  queue<int> edges;
+  rep(i, M) edges.push(tmp[i]);
+
+  rep(day, D) {
+    remain_days--;
+    // 今回修復する辺の番号の集合
+    set<int> repair;
+
+    // 既に確認済みの辺を管理する
+    vector<bool> checked(M);
+
+    while (int(repair.size()) < K) {
+      // 修理する辺がなくなったらループを抜ける
+      if (edges.size() == 0) {
+        break;
+      }
+      // 期限までに終了可能かつ必要な数を超えていればループを抜ける
+      // if (is_finishable(int(edges.size()), remain_days) &&
+      //     int(repair.size()) >= average) {
+      //   break;
+      // }
+
+      int front = edges.front();
+      edges.pop();
+
+      // TODO この辺りの処理を見直す
+
+      repair.insert(front);
+
+      // 既に同じ日に2度チェックしている場合は修理する辺に追加する
+      if (checked[front]) {
+        // 修理を終わらせられる場合は翌日に進む
+        if (is_finishable((int)edges.size(), remain_days)) {
+          break;
+        }
+        // それ以外ならばさらに辺を追加する
+        continue;
+      }
+
+      // グラフが連結でない場合はその辺は使用しない
+      if (!is_connected(repair)) {
+        repair.erase(front);
+        edges.push(front);
+        checked[front] = true;
+      }
+    }
+
+    // 修理する日を登録する
+    for (auto e : repair) {
+      result[e] = day + 1;
+    }
+  }
+
+  return result;
+}
+
+// ==================================================
+
+void input() {
   cin >> N >> M >> D >> K;
   g.resize(N);
-  repaired = vector<bool>(M);
+  connections.resize(M);
   rep(i, M) {
     int u, v, w;
     cin >> u >> v >> w;
     u--, v--;
+    connections[i] = {u, v};
     edge e;
     e.to = v, e.cost = w;
     g[u].push_back(e);
@@ -90,9 +215,31 @@ int main() {
     int _x, _y;
     cin >> _x >> _y;
   }
+}
 
-  // Solve
-  solve();
+void output(const vector<int>& result) {
+  assert(int(result.size()) == M);
+  rep(i, M) {
+    cout << result[i];
+    if (i == M - 1) {
+      cout << endl;
+    } else {
+      cout << " ";
+    }
+  }
+}
+
+// ==================================================
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(0);
+
+  input();
+
+  vector<int> result = solve();
+
+  output(result);
 
   return 0;
 }
---
+++ a/test.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+SEEDS=seeds.txt
+MAIN=main.cpp
+EXE=main
+SUM=sum.txt
+
+# Generate Inputs
+if [ $1 = "gen" ]; then
+	if [ $# -ne 2 ]; then
+		echo "Usage: ./test.sh gen \$num_inputs"
+		exit 1
+	fi
+	
+	echo "Create seeds.txt"
+	if [ -e $SEEDS ]; then
+		rm $SEEDS
+	fi
+	touch $SEEDS
+	
+	# input seeds
+	seq 0 $2 > $SEEDS
+	
+	echo "Move to the tools directory"
+	cd ./tools/
+	./target/release/gen ../$SEEDS
+	
+	cd ..
+	rm $SEEDS
+elif [ $1 = "test" ]; then
+	if [ ! -e $EXE ]; then
+		echo "Compiling ..."
+		g++ -o $EXE $MAIN
+	fi
+	
+	touch $SUM
+	echo "Move to the tools directory"
+	cd ./tools/
+	
+	for file in `ls ./in`; do
+		echo "testing $file ..."
+		../$EXE < ./in/$file > ./out/$file 2>/dev/null
+
+		if [ $? -ne 0 ]; then
+			echo "Test unsuccessfully finished..."
+			rm ../$SUM
+			exit 1
+		fi
+		./target/release/vis ./in/$file ./out/$file >> ../$SUM 
+	done
+
+	echo "Sum scores"
+	cd ..
+	awk '{sum+=$1}END{print "Sum", sum}' < $SUM
+	rm $SUM
+else
+	echo "I do not know that command."
+fi
+
+# TODO : generate table and records the score and the description for each test
